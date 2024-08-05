952bb4f307ab5478285ccdd0eef30930428a26bf
ashmeet420-69 <ashmeets424@gmail.com>
2024-08-05 15:52:07 +0530

    Sliding window started

---
+++ src/main/java/DSA/SlidingWindow.java
@@ -0,0 +1,37 @@
+package DSA;
+
+public class SlidingWindow {
+
+    public void maxSumInWindowOfSizeK(int[] arr,int k){
+        int mxsum=Integer.MIN_VALUE;
+        int sum=0;
+
+        if(k>arr.length || k== arr.length){
+            for(int i=0;i<arr.length;i++){
+                sum+=arr[i];
+            }
+            System.out.println(sum);
+            return;
+        }
+
+        for(int i=0;i<k;i++){
+            sum+=arr[i];
+        }
+
+        for(int i=k;i<arr.length;i++){
+            int j=i-k+1;
+            if(sum-arr[j-1]+arr[i]>mxsum){
+                mxsum=sum-arr[j-1]+arr[i];
+                sum=mxsum;
+            }
+        }
+
+        System.out.println(mxsum);
+
+    }
+
+    public static void main(String[] args) {
+        SlidingWindow window=new SlidingWindow();
+        window.maxSumInWindowOfSizeK(new int[]{2,1,8,4,5,9,9,1},3);
+    }
+}
