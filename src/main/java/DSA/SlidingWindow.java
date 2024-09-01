63377915f613429ecc15cbb0941983f3b5ccfcc5
ashmeet420-69 <ashmeets424@gmail.com>
2024-09-01 14:47:47 +0530

    Sliding window questions practice

--- src/main/java/DSA/SlidingWindow.java
+++ src/main/java/DSA/SlidingWindow.java
@@ -1,9 +1,6 @@
 package DSA;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 import java.util.concurrent.ArrayBlockingQueue;
 
 public class SlidingWindow {
@@ -12,33 +9,53 @@ public class SlidingWindow {
         int mxsum=Integer.MIN_VALUE;
         int sum=0;
 
-        if(k>arr.length || k== arr.length){
-            for(int i=0;i<arr.length;i++){
-                sum+=arr[i];
-            }
-            System.out.println(sum);
-            return;
-        }
-
-        for(int i=0;i<k;i++){
+        for(int i=0;i<k  && i<arr.length;i++){
             sum+=arr[i];
         }
-
-        for(int i=k;i<arr.length;i++){
+        int i=0;
+        if(k<arr.length) i=k;
+        else{
+            System.out.println(Arrays.stream(arr).sum());
+            return;
+        }
+        for(;i<arr.length;i++){
             int j=i-k+1;
             if(sum-arr[j-1]+arr[i]>mxsum){
                 mxsum=sum-arr[j-1]+arr[i];
                 sum=mxsum;
             }
         }
-
-        System.out.println(mxsum);
-
+        System.out.print(mxsum);
     }
 
+    public void longestSubarrayWithSumLessOrEqualToK(int[] arr, int k){
+        int i=0;
+        int j=0;
+        int mxLen=Integer.MIN_VALUE;
+        int sum=0;
+        while(i<arr.length && j<arr.length){
+            if(sum>k){
+                sum-=arr[j];
+                j++;
+            } else {
+                sum+=arr[i];
+                if(sum <= k){
+                    mxLen=Math.max(mxLen,i-j+1);
+                }
+                i++;
+            }
+
+        }
+
+        System.out.print(mxLen);
+    }
 
     public static void main(String[] args) {
         SlidingWindow window=new SlidingWindow();
+        System.out.println("Maximum sum in windows of size K: ");
         window.maxSumInWindowOfSizeK(new int[]{2,1,8,4,5,9,9,1},3);
+
+        System.out.println("\nLongest Subarray with Sum less than or equal to K: ");
+        window.longestSubarrayWithSumLessOrEqualToK(new int[]{1,1,8,4,5,9,9,1},2);
     }
 }
