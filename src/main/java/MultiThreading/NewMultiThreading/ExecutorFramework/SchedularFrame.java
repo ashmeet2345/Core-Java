733a1192db3e8071e21f3b11efc6bace36963ab8
ashmeet420-69 <ashmeets424@gmail.com>
2024-09-07 00:15:11 +0530

    ScheduledExecutorService

---
+++ src/main/java/MultiThreading/NewMultiThreading/ExecutorFramework/SchedularFrame.java
@@ -0,0 +1,16 @@
+package MultiThreading.NewMultiThreading.ExecutorFramework;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class SchedularFrame {
+    public static void main(String[] args) {
+        ScheduledExecutorService schedular=Executors.newScheduledThreadPool(2);
+        schedular.scheduleAtFixedRate(()-> System.out.println("Hello"),5,5, TimeUnit.SECONDS);
+        schedular.schedule(()->{
+            System.out.println("Initiating shutdown");
+            schedular.shutdown();
+        },20,TimeUnit.SECONDS);
+    }
+}
