b23749de6db04c1553509e8a3ed0eb4f68dd2ad3
ashmeet420-69 <ashmeets424@gmail.com>
2024-09-12 16:04:26 +0530

    Java 8 functionalities

---
+++ src/main/java/Java8/FunctionaInterface.java
@@ -0,0 +1,11 @@
+package Java8;
+
+public interface FunctionaInterface {
+    //abstract void single();
+    //A funtional interface is interface having only 1 abstract method but can have number of
+    // default and static methods.
+    //Example - Runnable, Callable, Comparable, Comparator
+
+    void m1();
+
+}
---
+++ src/main/java/Java8/LambdaExpression.java
@@ -0,0 +1,17 @@
+package Java8;
+
+public class LambdaExpression{
+    //Expression through which we can represent an anonymous function
+    //It is applicable only for functonal interfaces
+    //If the interface only contains one abstract method, then we don't need to implement it.
+
+    public static void main(String[] args) {
+        //to convert a function interface into a lambda expression (parameters)   ->  {body}
+
+        FunctionaInterface face=() -> {
+            System.out.println("Hello, I am the only abstract method in the interface.");
+        };
+
+        face.m1();
+    }
+}
