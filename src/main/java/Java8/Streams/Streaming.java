ab29b56a36d877f41a7ff8b8c1e0e4390cac5738
ashmeet420-69 <ashmeets424@gmail.com>
2024-09-22 20:58:55 +0530

    Streams questions

--- src/main/java/Java8/Streams/Streaming.java
+++ src/main/java/Java8/Streams/Streaming.java
@@ -8,10 +8,12 @@ import lombok.ToString;
 import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
 
@@ -61,6 +63,46 @@ public class Streaming {
         return ans;
     }
 
+    public void occurenceOfEachCharacter(String str){
+        Map<String, Long> mp=Arrays.stream(str.split(""))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        System.out.println(mp);
+    }
+
+    public void allDuplicateElementsFromAGivenString(String str){
+        List<String> list=Arrays.stream(str.split(""))
+                .collect(groupingBy(identity(), counting()))
+                .entrySet().stream().filter(k->k.getValue()>1).map(Map.Entry::getKey).collect(Collectors.toList());
+
+        System.out.println(list);
+    }
+
+    public void findFirstNonRepeatingElement(String str){
+        String st=Arrays.stream(str.split(""))
+                .collect(Collectors.groupingBy(Function.identity(),LinkedHashMap::new,Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(s->s.getValue()==1)
+                .findFirst().get().getKey();
+        System.out.println(st);
+    }
+
+    public void find2ndHightValueInArray(int[] arr){
+        Integer res=Arrays.stream(arr).boxed()
+                .sorted(Collections.reverseOrder())
+                .skip(1)
+                .findFirst()
+                .get();
+        System.out.println(res);
+    }
+
+    public void longestStringInAGivenArray(String[] arr){
+        String str=Arrays.stream(arr)
+                .reduce((word1,word2)->word1.length()>word2.length()?word1:word2)
+                .get();
+        System.out.println(str);
+    }
+
     public static void main(String[] args) {
         List<User> users=new ArrayList<>();
         users.add(new User("user1","12345",List.of("abc@xyz","cde@fgh")));
@@ -76,7 +118,7 @@ public class Streaming {
         String s="Ashmeet";
 
         Map<String,Long> mp= Arrays.stream(s.split(""))
-                .collect(groupingBy(Function.identity(),counting()));
+                .collect(groupingBy(identity(),counting()));
 
         System.out.println(mp);
 
@@ -115,6 +157,33 @@ public class Streaming {
         System.out.println();
         List<String> words = Arrays.asList("java", "streams", "are", "powerful");
         words.stream().map(t -> t.toUpperCase()).forEach(x -> System.out.print(x+" "));
+        System.out.println();
 
+        List<Integer> list=Arrays.asList(9,4,1,6,3,2,7,8,5);
+        //if we want to print only limited elements from the list, then we use skip and limit
+
+        list.stream().skip(1).limit(5).sorted().forEach(st-> System.out.print(st+" "));
+        //above 1 mentioned in skip method means, skip the first element of the list,
+        //and 5 mentioned in the limit method means, after skipping the 1st index, we count till 5th
+        //element and print the elements.
+        //If we want to skip first 2 elements, then we mention 2 in the parameter of the skip method.
+
+        Streaming streaming=new Streaming();
+        System.out.println();
+        streaming.occurenceOfEachCharacter("ashmeetsingh");
+
+        System.out.println();
+        streaming.allDuplicateElementsFromAGivenString("ashmeetsingh");
+
+        System.out.println();
+        streaming.findFirstNonRepeatingElement("ashmeetsingh");
+
+        System.out.println();
+        streaming.find2ndHightValueInArray(new int[]{2,11,99,32,48,57,69});
+
+        System.out.println();
+        streaming.longestStringInAGivenArray(new String[]{"abc","asdasd","dasd","asasdfjjweieijfjn"});
     }
+
+
 }
