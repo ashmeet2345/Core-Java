638bf059da0d34088c523ead84f76a0524f74e88
ashmeet420-69 <ashmeets424@gmail.com>
2024-10-07 16:57:19 +0530

    Longest palindromic subsequence

--- src/main/java/DSA/DynamicProgrammingLevel2.java
+++ src/main/java/DSA/DynamicProgrammingLevel2.java
@@ -385,13 +385,17 @@ public class DynamicProgrammingLevel2 {
     public void longestPalindromicSubsequence(String s){
         int[][] dp=new int[s.length()][s.length()];
         for(int g=0;g<s.length();g++){
-            for(int i=0,j=g;j<dp.length;i++,j++){
+            for(int i=0,j=g;j<s.length();i++,j++){
                 if(g==0){
                     dp[i][j]=1;
-                }else if (g==1){
-                    dp[i][j]=s.charAt(i)==s.charAt(j)?2:1;
-                }else {
-                    if(s.charAt(i) == s.charAt(j)){
+                }else if(g==1){
+                    if(s.charAt(i)==s.charAt(j)){
+                        dp[i][j]=2;
+                    }else{
+                        dp[i][j]=1;
+                    }
+                }else{
+                    if(s.charAt(i)==s.charAt(j)){
                         dp[i][j]=2+dp[i+1][j-1];
                     } else {
                         dp[i][j]=Math.max(dp[i][j-1],dp[i+1][j]);
