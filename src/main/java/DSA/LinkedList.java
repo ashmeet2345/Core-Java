658c5c5f862d4021c57b19e5df76a99fe3fa8cba
ashmeet420-69 <ashmeets424@gmail.com>
2024-10-21 18:23:44 +0530

    Linked list try

--- src/main/java/DSA/LinkedList.java
+++ src/main/java/DSA/LinkedList.java
@@ -1,137 +1,81 @@
 package DSA;
 
 public class LinkedList {
-    public static class Node{
+
+    public class Node{
         int data;
         Node next;
 
-        public Node(int data) {
-            this.data = data;
-            this.next = null;
+        public Node(int data){
+            this.data=data;
+        }
+
+        public Node(int val, Node next){
+            this.data=val;
+            this.next=next;
         }
     }
-    Node head=null;
-    Node tail=null;
-    int size=0;
 
-
-    public void insertFirst(int data){
-        Node temp=new Node(data);
-        if(head==null){
-            head=tail=temp;
-        } else {
-            temp.next=head;
-            head=temp;
-        }
-        size++;
-    }
+    public static Node head=null;
+    public static Node tail=null;
+    public static int size=0;
 
     public void insertLast(int data){
-        Node temp=new Node(data);
         if(head==null){
-            head=tail=temp;
+            Node node=new Node(data);
+            head=node;
+            tail=node;
+            return;
         } else {
-            tail.next=temp;
-            tail=temp;
+            Node node=new Node(data);
+            tail.next=node;
+            tail=node;
         }
         size++;
     }
 
-    public void insertAtK(int data,int k){
-        Node temp=new Node(data);
-        Node temp2=head;
-        for(int i=1;i<=k-2;i++){
-            temp2=temp2.next;
+    public void insertFirst(int data){
+        if(head==null){
+            Node node=new Node(data);
+            head=node;
+            tail=node;
+            return;
+        } else {
+            Node node=new Node(data);
+            node.next=head;
+            head=node;
         }
-        temp.next=temp2.next;
-        temp2.next=temp;
+        size++;
+    }
+
+    public void insertAtKPosition(int data,int k){
+        Node temp=head;
+        Node node=new Node(data);
+        for(int i=0;i<k-2;i++){
+            temp=temp.next;
+        }
+        node.next=temp.next;
+        temp.next=node;
+        size++;
     }
 
     public void display(){
         Node temp=head;
         while(temp!=null){
-            System.out.print(temp.data+"->");
+            System.out.println(temp.data+" ");
             temp=temp.next;
         }
     }
 
-    public void reverseHelper(Node node){
-        if(node.next==null){
-            return;
-        }
-        reverseHelper(node.next);
-        node.next.next=node;
-    }
-
-    public void reverseRecursive(Node head){
-        reverseHelper(head);
-        head.next=null;
-        Node temp=head;
-        head=tail;
-        tail=temp;
-        display();
-    }
-
-    public void reverse(){
-        Node cur=head;
-        Node prev=null;
-        while(cur!=null){
-            Node next=cur.next;
-            cur.next=prev;
-            prev=cur;
-            cur=next;
-        }
-        head=prev;
-    }
-
-    public void foldHelper(Node right,int mid){
-        if(right==null){
-            return;
-        }
-        foldHelper(right.next,mid+1);
-        if(mid > size/2){
-            Node temp=left.next;
-            left.next=right;
-            right.next=temp;
-            left=temp;
-        } else if(mid == size/2){
-            tail=right;
-            tail.next=null;
-        }
-    }
-
-    Node left;
-    public void fold(){
-        left=head;
-        foldHelper(head,0);
-    }
-
-    public void unfold(){
-        Node temp1=head;
-        Node temp2=head.next;
-        Node h1=head;
-        Node h2=head.next;
-        while(temp1.next.next!=null){
-            temp1.next=temp1.next.next;
-        }
-        while(temp2.next.next!=null){
-            temp2.next=temp2.next.next;
-        }
-    }
-
     public static void main(String[] args) {
-
-        LinkedList l=new LinkedList();
-        l.insertFirst(10);
-        l.insertFirst(20);
-        l.insertFirst(30);
-        l.insertLast(40);
-        l.insertLast(50);
-        l.insertLast(60);
-        l.insertAtK(15,3);
-        l.display();
-        l.reverse(); System.out.println(); l.display();
-        l.fold(); System.out.println(); l.display();
+        LinkedList list=new LinkedList();
+        list.insertFirst(10);
+        list.insertFirst(20);
+        list.insertFirst(30);
+        list.insertFirst(40);
+        list.insertFirst(50);
+        list.insertAtKPosition(80,3);
+        list.display();
 
     }
 }
